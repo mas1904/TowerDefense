6b0a1eb310ecd0fb7e5a9965603ed18dc14de034
mas\Łukasz <mas1904@gmail.com>
2018-02-06 15:12:46 +0100

    first commit

---
+++ include/Tower.h
@@ -0,0 +1,18 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+
+class Tower
+{
+    public:
+        /** Default constructor */
+        Tower();
+        /** Default destructor */
+        virtual ~Tower();
+
+    protected:
+
+    private:
+};
+
+#endif // TOWER_H
